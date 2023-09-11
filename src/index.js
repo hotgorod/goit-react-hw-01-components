460ddd68c8aa58a8fb0231eaeda4540fd0fd18f2
hotgorod <hotgorod@gmail.com>
2023-09-11 17:59:05 +0100

    component Profile created without css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './user.json'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Profile = () => { 
+  
+    return <div className="profile">
+  <div className="description">
+    <img
+      src={user.avatar}
+      alt="User avatar"
+      className="avatar"
+    />
+    <p className="name">{user.username}</p>
+    <p className="tag">@{user.tag}</p>
+    <p className="location">{user.location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{user.stats.followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{user.stats.views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{user.stats.likes}</span>
+    </li>
+  </ul>
+</div>
+  
+
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Profile />);
